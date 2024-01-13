15a5fcb79d20567108d3629808a63b42234957a0
venugopalgithub1991 <156532909+venugopalgithub1991@users.noreply.github.com>
2024-01-14 01:19:39 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "HELLOWORLD"
 echo "Hi there a change on script"
-echo "hi I have added the auto build using webhook and trigered email using SMTP"
+echo "hi I have added the auto build using webhook and trigered email using SMTP enabled"
